3ea00cdd7e3714513962cc1089b389fbe1fc10f1
juliaporfiro <julia.porfiro@germinare.org.br>
2024-10-22 09:15:21 -0300

    criando classe Usuario.java, UsuarioDAO.java, UsuariosAtivos.java Criando jsp selectUsuariosA.jsp

---
+++ src/main/java/Daos/Model/Usuario.java
@@ -0,0 +1,101 @@
+package Daos.Model;
+
+import java.sql.Date;
+
+public class Usuario {
+
+    String textFoto;
+    String varEmail;
+    String senha;
+    String userName;
+    Date dt_nascimento;
+    String descricaoUsua;
+    Long cpf;
+    String nome;
+    int fk_int_id_endereco;
+
+    public Usuario(String textFoto, String varEmail, String senha, String userName, Date dt_nascimento, String descricaoUsua,
+                   Long cpf, String nome, int fk_int_id_endereco) {
+        this.textFoto = textFoto;
+        this.varEmail = varEmail;
+        this.senha = senha;
+        this.userName = userName;
+        this.dt_nascimento = dt_nascimento;
+        this.descricaoUsua = descricaoUsua;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.fk_int_id_endereco = fk_int_id_endereco;
+    }
+
+    public String getTextFoto() {
+        return textFoto;
+    }
+
+    public void setTextFoto(String textFoto) {
+        this.textFoto = textFoto;
+    }
+
+    public String getVarEmail() {
+        return varEmail;
+    }
+
+    public void setVarEmail(String varEmail) {
+        this.varEmail = varEmail;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getDt_nascimento() {
+        return dt_nascimento;
+    }
+
+    public void setDt_nascimento(Date dt_nascimento) {
+        this.dt_nascimento = dt_nascimento;
+    }
+
+    public String getDescricaoUsua() {
+        return descricaoUsua;
+    }
+
+    public void setDescricaoUsua(String descricaoUsua) {
+        this.descricaoUsua = descricaoUsua;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getFk_int_id_endereco() {
+        return fk_int_id_endereco;
+    }
+
+    public void setFk_int_id_endereco(int fk_int_id_endereco) {
+        this.fk_int_id_endereco = fk_int_id_endereco;
+    }
+}
