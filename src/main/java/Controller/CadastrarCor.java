f7143a31dde479e89bf6b53a6b5fe2468326abd8
isabelaneu <isabelaneu05@gmail.com>
2024-10-25 08:54:25 -0300

    métodos AtualizarCorMascote e ListarCorMascote

--- src/main/java/Controller/CadastrarCor.java
+++ src/main/java/Controller/CadastrarCor.java
@@ -1,4 +1,19 @@
 package Controller;
 
-public class CadastrarCor {
-}//De acordo com a requisição do usuário cadastrar cor
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "CadastroBarraca" , value = "/tabCadastrarBarraca")
+public class CadastrarCor extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+
+}
