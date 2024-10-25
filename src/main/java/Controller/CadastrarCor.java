de6a23e95db15b102ab9c8e4b2caac7dc4cb7b2a
isabelaneu <isabelaneu05@gmail.com>
2024-10-25 15:23:40 -0300

    JSP - cadastrar, atualizar e deletar

--- src/main/java/Controller/CadastrarCor.java
+++ src/main/java/Controller/CadastrarCor.java
@@ -1,19 +1,48 @@
 package Controller;
 
+import Daos.JDBC.CorMascoteDAO;
+import Model.CorMascote;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.*;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
-@WebServlet(name = "CadastroBarraca" , value = "/tabCadastrarBarraca")
+@WebServlet(name = "CadastroCor" , value = "/tabCadastrarCor")
 public class CadastrarCor extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
+        String textFundo =req.getParameter("text_fundo");
+        String textPrimaria = req.getParameter("text_primaria");
+        String textSecundaria = req.getParameter("text_secundaria");
+
+        CorMascoteDAO corMascoteDAO = new CorMascoteDAO();
+        CorMascote corMascote = new CorMascote(textFundo,textPrimaria,textSecundaria);
+
+        if (textFundo.matches("^#([A-Fa-f0-9]{6})$")) {
+
+            if (corMascoteDAO.cadastrarCorMascote(corMascote)) {
+                req.getRequestDispatcher("telasadmin.jsp").forward(req, resp);
+            }
+            else {
+                req.setAttribute("resultado","Não foi possível cadastrar!");
+                req.getRequestDispatcher("cadastrarCor.jsp").forward(req, resp);
+            }
+        } else {
+            req.setAttribute("resultado","Você digitou o código de cor RGB de maneira errada!");
+            req.getRequestDispatcher("cadastrarCor.jsp").forward(req, resp);
+        }
+
+
+
+
+
+
+
     }
-
-
 }
---
+++ src/main/webapp/Pages/cadastrarCorMascote.jsp
@@ -0,0 +1,48 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: isabelaneu-ieg
+  Date: 25/10/2024
+  Time: 09:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSSdosAps/telasadm.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+
+    <title>Telas de Administrador</title>
+</head>
+<body>
+
+<main>
+    <div class="container2" id="tabNovaCor">
+        <div class="titulo">
+            <h1>CADASTRAR COR DO MASCOTE</h1>
+        </div>
+        <section class="cor">
+            <form action="mascote">
+                <input type="text" name="text_fundo" id="text_fundo" placeholder="Cor do fundo" required>
+                <input type="text" name="text_secundaria" id="text_secundaria" placeholder="Cor secundária" required>
+                <input type="text" name="text_primaria" id="text_primaria" placeholder="Cor primária" required>
+                <button type="submit">Cadastrar</button>
+            </form>
+        </section>
+    </div>
+</main>
+
+<footer class="container3">
+    <p>Desenvolvido por</p>
+    <img src="../im/mocBrancaSemFUndo.png" alt="img6" width="10%">
+</footer>
+
+</body>
+</html>
+
+
