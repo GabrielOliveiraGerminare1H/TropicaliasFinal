42dab240e4d98d9e18056b673a0c252af8a35060
maiteluiza <maite.pereira@germinare.org.br>
2024-10-25 08:22:05 -0300

    Comentários em CadastrarEvento e telasadmin atualizado

--- src/main/java/Controller/CadastrarEvento.java
+++ src/main/java/Controller/CadastrarEvento.java
@@ -1,5 +1,6 @@
 package Controller;
-
+//Fazer a verificação das datas, verificar se a data inicial é maior que a data final,
+// e verificar se o preço está correto,,,, caso dê erro reenvia para a mesma página jsp com a mensagem do que errou!
 import java.io.*;
 
 import jakarta.servlet.ServletException;
@@ -9,7 +10,6 @@ import jakarta.servlet.annotation.*;
 @WebServlet(name = "evento", value = "/evento")
 public class CadastrarEvento extends HttpServlet {
     @Override
-
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         String dataini = req.getParameter("dataini");
@@ -18,19 +18,14 @@ public class CadastrarEvento extends HttpServlet {
         String local = req.getParameter("local");
         String preco = req.getParameter("preco");
 
-        if () {
-
-            if () {
-
-
-            } else {
-                req.setAttribute();
-            }
-
+        if (preco.matches("[0-9]+([,.][0-9]+)?")) {
+            double precoDouble = Double.parseDouble(preco);
         } else {
-            req.setAttribute();
-            req.getRequestDispatcher("Login.jsp").forward(req, resp);
+            req.setAttribute("resultado","Você digitou o preço de maneira errada! ");
+            req.getRequestDispatcher("cadastrarEvento.jsp").forward(req, resp);
         }
+
+
     }
 
     public void destroy() {
