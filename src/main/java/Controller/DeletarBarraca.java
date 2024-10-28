7f55fd40aec43b3fd634ef8fe992263f69272002
juliaporfiro <julia.porfiro@germinare.org.br>
2024-10-28 08:05:39 -0300

    barracas concluidas e modificação html

--- src/main/java/Controller/DeletarBarraca.java
+++ src/main/java/Controller/DeletarBarraca.java
@@ -1,6 +1,43 @@
 package Controller;
 
-public class DeletarBarraca {
+import Daos.JDBC.BarracaDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "DeletarBarraca", value = "/tabDeletarBarracaBarraca")
+public class DeletarBarraca extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response )
+            throws ServletException, IOException {
+        try{
+            int idBarraca = Integer.parseInt(request.getParameter("idBarraca"));
+            BarracaDAO barracaDAO = new BarracaDAO();
+            boolean verifica = barracaDAO.softDeleteBarraca(idBarraca);
+
+            String mensagem = verifica ? "Barraca deletada com sucesso!" : "Erro ao deletar barraca.";
+
+            request.setAttribute("verifica",verifica);
+            request.setAttribute("mensagem",mensagem);
+
+            request.getRequestDispatcher("mensagem.jsp").forward(request,response);
+
+
+
+        }catch (NumberFormatException nfe){
+           request.setAttribute("verifica",false);
+            request.setAttribute("mensagem",nfe.getMessage());
+            request.getRequestDispatcher("mensagem.jsp").forward(request,response);
+
+        }
+
+
+
+    }
+
 }
 
-//Com a requisição do usuário deletar barraca
---
+++ src/main/webapp/mensagem.jsp
@@ -0,0 +1,21 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Mensagem</title>
+
+</head>
+
+<body>
+<jsp:include page="telasadmin.html">
+</jsp:include>>
+
+<%
+    Boolean verifica = (Boolean) request.getAttribute("verifica");
+    String mensagem = (String) request.getAttribute("mensagem");
+    String cor = verifica ? "green" : "red";
+%>
+
+<p style="color: <%= cor %>"> MENSAGEM:<%=mensagem%></p>
+
+</body>
+</html>
