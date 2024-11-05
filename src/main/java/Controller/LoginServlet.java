68ac56bb6747b571d1a4a73d1fdb9852126ca16e
GabrielOliveiraGerminare1H <157626061+GabrielOliveiraGerminare1H@users.noreply.github.com>
2024-11-05 07:11:57 -0300

    Comitando Login fe

--- src/main/java/Controller/LoginServlet.java
+++ src/main/java/Controller/LoginServlet.java
@@ -36,28 +36,33 @@ public class LoginServlet extends HttpServlet {
             request.setAttribute("resultado", "E-mail ou senha inválidos!");
             request.getRequestDispatcher("/Pages/Login.jsp").forward(request, response);
 
+
         } else if (conn.getResponseCode() == 403) {
             // Caso o usuário não tenha autorização para acessar o sistema
             request.setAttribute("resultado", "Usuário não autorizado!");
-            request.getRequestDispatcher("/Pages/Login.jsp").forward(request, response);
+            request.getRequestDispatcher(request.getContextPath() + "/Pages/Login.jsp").forward(request, response);
+
 
         } else if (conn.getResponseCode() == 404) {
             // Caso o usuário não seja encontrado na base de dados
             request.setAttribute("resultado", "Usuário não encontrado!");
-            request.getRequestDispatcher("/Pages/Login.jsp").forward(request, response);
+            request.getRequestDispatcher(request.getContextPath() + "/Pages/Login.jsp").forward(request, response);
+
         } else if (conn.getResponseCode() == 418) {
             // Resposta personalizada do servidor para indicar uma condição especial
             request.setAttribute("resultado", "Você é muito especial!");
-            request.getRequestDispatcher("/Pages/Login.jsp").forward(request, response);
+            request.getRequestDispatcher(request.getContextPath() + "/Pages/Login.jsp").forward(request, response);
+
 
         } else if (conn.getResponseCode() == 500) {
             // Caso ocorra um erro interno no servidor
             request.setAttribute("resultado", "Ocorreu um erro interno do servidor! Tente novamente mais tarde!");
-            request.getRequestDispatcher("/Pages/Login.jsp").forward(request, response);
+            request.getRequestDispatcher(request.getContextPath() + "/Pages/Login.jsp").forward(request, response);
+
         }
 
         // Lê a resposta da API
-        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+//        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
 
         // Desconecta a conexão
         conn.disconnect();
